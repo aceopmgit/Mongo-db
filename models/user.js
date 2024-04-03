9ebeb0344c6a803dc4a28ffb1e6586618faa5055
aceopmgit <ace.opm.sales@gmail.com>
2024-04-03 13:16:15 +0530

    EX-Deleting Cart Items

--- models/user.js
+++ models/user.js
@@ -38,8 +38,37 @@ class User {
 
     const updatedCart = { items: updatedCartItems };
     const db = getDb();
-    db.collection('users')
+    return db.collection('users')
       .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: updatedCart } })
+
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    })
+    return db.collection('products').find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map(p => {
+          return {
+            ...p, quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString()
+            }).quantity
+          }
+        })
+      })
+  }
+
+  deleteItemFromCart(prodId) {
+    const updatedCartItems = this.cart.items.filter((i) => {
+      return i.productId.toString() !== prodId.toString();
+    });
+
+    const db = getDb();
+    return db.collection('users')
+      .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: { items: updatedCartItems } } })
 
   }
 
@@ -55,19 +84,6 @@ class User {
       })
   }
 }
-// const Sequelize = require('sequelize');
 
-// const sequelize = require('../util/database');
-
-// const User = sequelize.define('user', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   name: Sequelize.STRING,
-//   email: Sequelize.STRING
-// });
 
 module.exports = User;
